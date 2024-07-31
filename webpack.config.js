2a8f565eb23f8fb5f5a01bfbcbde11f817521f4c
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-07-31 18:45:00 -0400

    add Prettier, image loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: { static: "./dist", watchFiles: [".src/**"] },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: { static: './dist', watchFiles: ['.src/**'] },
   plugins: [new HtmlWebpackPlugin({})],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
