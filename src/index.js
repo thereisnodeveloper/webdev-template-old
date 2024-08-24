59852c09b01d20a10e1b24d552178db71f251f41
thereisnodeveloper <56517440+thereisnodeveloper@users.noreply.github.com>
2024-08-24 18:24:11 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './reset.css';
 import './style.css';
 
 const testElem = document.createElement("div")
+function isRequired(argument) {
+  throw new Error(`missing argument: ${argument}`);
+}
